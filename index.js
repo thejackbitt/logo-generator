6224680a879bd8a4cae8edcbc00c15b86ac57b3f
thejackbitt <jack.bittner@outlook.com>
2024-01-26 17:20:33 -0600

    added main functionality

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const inquirer = require('inquirer');
-
-function writeText(t) {
-    console.log('The text for the logo is ' + t)
-}
-
-function writeColor(tc) {
-    console.log('The text color for the logo is ' + tc)
-}
+const fs = require('fs');
 
 function writeShape(s) {
-    console.log('The shape for the logo is ' + s)
-}
-
-function writeShapeColor(sc) {
-    console.log('The shape color for the logo is ' + sc)
-}
+    // console.log('The shape for the logo is ' + s)
+    if (s === 'square') {
+        return '<rect x="50.5" y="0.5" class="st0" width="199" height="199"/>'
+    } else if (s === 'circle') {
+        return '<circle class="st0" cx="150.5" cy="99.5" r="99.5"/>'
+    } else if (s === 'triangle') {
+        return '<polygon class="cls-2" points="250 200 50 200 150 0 250 200"/>'
+    }
+};
 
 inquirer
     .prompt([
@@ -47,10 +43,30 @@ inquirer
     .then((response) => {
             const { text, tColor, shape, sColor } = response;
 
-        writeText(text);
-        writeColor(tColor);
-        writeShape(shape);
-        writeShapeColor(sColor);
+        const shapeCode = writeShape(shape);
 
+        fs.writeFile(
+            `./logo.svg`,
+            `<?xml version="1.0" encoding="utf-8"?>
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox="0 0 300 200" style="enable-background:new 0 0 300 200;" xml:space="preserve">
+            <style type="text/css">
+                .st0{fill:${sColor};}
+                .st1{fill:none;}
+                .st2{fill:${tColor};}
+                .st3{font-family:'Verdana-Bold';}
+                .st4{font-size:115.7074px;}
+            </style>
+            ${shapeCode}
+            <rect y="40" class="st1" width="300" height="135"/>
+            <text transform="matrix(1 0 0 1 27.0894 139.915)" class="st2 st3 st4">${text}</text>
+            </svg>`,
+            function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                    console.log('SVG logo created successfully.');
+                }
+        )
     })
     
